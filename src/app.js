9af1055091a8e17448af232f155c985f013a6214
Fredy Alexander Camelo <88693018+alex2219@users.noreply.github.com>
2026-02-03 15:10:20 +0000

    generador de dominios finalizado

--- src/app.js
+++ src/app.js
@@ -1,11 +1,12 @@
-import "bootstrap";
-import "./style.css";
+let pronoun = ['the', 'our'];
+let adj = ['great', 'big'];
+let noun = ['jogger', 'racoon'];
 
+for (let pro of pronoun){
+  for(let ad of adj){
+    for(let nou of noun){
+      console.log(`${pro}${ad}${nou}.com`);
+    }
+  }
+}
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
-
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
-};
